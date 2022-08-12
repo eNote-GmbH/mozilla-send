8f1423030db07b4788782dc8190cccdf04e54a3e
Aleksandr Saiapin <alstutor@gmail.com>
2022-08-12 15:55:26 +0700

    chore: refactor upload procedures - refs #BAC-2037

--- server/routes/resumable_upload_done.js
+++ server/routes/resumable_upload_done.js
@@ -1,52 +1,17 @@
 const createReadStream = require('fs').createReadStream;
 
-const crypto = require('crypto');
-const storage = require('../storage');
-const config = require('../config');
-const mozlog = require('../log');
 const Limiter = require('../limiter');
 const { encryptedSize } = require('../../app/utils');
 
-const log = mozlog('send.upload');
+const fileStreamUpload = function(req, res, config) {
+  const limiter = new Limiter(encryptedSize(config.max_file_size));
+  const fileName = '.'.concat(config.resumable_file_dir, '/', req.params.id);
+  const sourceFileStream = createReadStream(fileName);
+  return sourceFileStream.pipe(limiter);
+};
+
+const uploader = require('../utils');
 
 module.exports = async function(req, res) {
-  const newId = crypto.randomBytes(8).toString('hex');
-  const metadata = req.header('X-File-Metadata');
-  const auth = req.header('Authorization');
-  const contentType = req.header('Content-Type');
-  if (!metadata || !auth) {
-    return res.sendStatus(400);
-  }
-  const owner = crypto.randomBytes(10).toString('hex');
-  const meta = {
-    owner,
-    metadata,
-    auth: auth.split(' ')[1],
-    user: req.user.client_id,
-    nonce: crypto.randomBytes(16).toString('base64'),
-    contentType
-  };
-
-  try {
-    const limiter = new Limiter(encryptedSize(config.max_file_size));
-    const fileName = '.'.concat(config.resumable_file_dir, '/', req.params.id);
-    const sourceFileStream = createReadStream(fileName);
-    const fileStream = sourceFileStream.pipe(limiter);
-    //this hasn't been updated to expiration time setting yet
-    //if you want to fallback to this code add this
-    await storage.set(newId, fileStream, meta, config.default_expire_seconds);
-    const url = `${config.deriveBaseUrl(req)}/download/${newId}/`;
-    res.set('WWW-Authenticate', `send-v1 ${meta.nonce}`);
-    res.json({
-      url,
-      owner: meta.owner,
-      id: newId
-    });
-  } catch (e) {
-    if (e.message === 'limit') {
-      return res.sendStatus(413);
-    }
-    log.error('upload', e);
-    res.sendStatus(500);
-  }
+  await uploader(req, res, fileStreamUpload);
 };
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,48 +1,13 @@
-const crypto = require('crypto');
-const storage = require('../storage');
-const config = require('../config');
-const mozlog = require('../log');
 const Limiter = require('../limiter');
 const { encryptedSize } = require('../../app/utils');
 
-const log = mozlog('send.upload');
+const uploader = require('../utils');
+
+const reqStreamUpload = function(req, res, config) {
+  const limiter = new Limiter(encryptedSize(config.max_file_size));
+  return req.pipe(limiter);
+};
 
 module.exports = async function(req, res) {
-  const newId = crypto.randomBytes(8).toString('hex');
-  const metadata = req.header('X-File-Metadata');
-  const auth = req.header('Authorization');
-  const contentType = req.header('Content-Type');
-  if (!metadata || !auth) {
-    return res.sendStatus(400);
-  }
-  const owner = crypto.randomBytes(10).toString('hex');
-  const meta = {
-    owner,
-    metadata,
-    auth: auth.split(' ')[1],
-    user: req.user.client_id,
-    nonce: crypto.randomBytes(16).toString('base64'),
-    contentType
-  };
-
-  try {
-    const limiter = new Limiter(encryptedSize(config.max_file_size));
-    const fileStream = req.pipe(limiter);
-    //this hasn't been updated to expiration time setting yet
-    //if you want to fallback to this code add this
-    await storage.set(newId, fileStream, meta, config.default_expire_seconds);
-    const url = `${config.deriveBaseUrl(req)}/download/${newId}/`;
-    res.set('WWW-Authenticate', `send-v1 ${meta.nonce}`);
-    res.json({
-      url,
-      owner: meta.owner,
-      id: newId
-    });
-  } catch (e) {
-    if (e.message === 'limit') {
-      return res.sendStatus(413);
-    }
-    log.error('upload', e);
-    res.sendStatus(500);
-  }
+  await uploader(req, res, reqStreamUpload);
 };
